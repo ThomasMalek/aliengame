2daf9cc97eeefe43694e7246804c681f32430935
ThomasMalek <thomasmalekmccarthy@gmail.com>
2020-05-05 14:49:58 -0400

    aliens game

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'gamepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Aliens', initialRoute: '/', routes: {
+      '/': (context) => MyHomePage(),
+      '/game': (context) => GamePage(),
+//      '/chooser': (context) => Chooser(),
+    });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.grey,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.grey,
+              ),
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
